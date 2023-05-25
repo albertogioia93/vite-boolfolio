3b8a1c949e6dac5295da7f065d89f5d7b9c9c3a4
albertogioia93 <albertogioia@hotmail.it>
2023-05-25 20:47:07 +0200

    axios importato + stampa in console dati api

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      apiBaseUrl: 'http://127.0.0.1:8000/api',
+      apiUrls: {
+        posts: '/posts'
+      },
+      posts: []
+    }
+  },
   components: {
     AppHeader,
     AppMain,
     AppFooter
+  },
+  methods: {
+    getPosts(){
+      axios.get(this.apiBaseUrl + this.apiUrls.posts)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+  },
+  created(){
+    this.getPosts();
   }
 }
 </script>
