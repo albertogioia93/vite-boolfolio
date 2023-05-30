7d6d7e40fb5e997d83e2394216084ac45234ea94
albertogioia93 <albertogioia@hotmail.it>
2023-05-30 15:03:04 +0200

    form di contatto con name e message

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,19 +1,56 @@
 <script>
 import axios from 'axios';
+import store from '../store';
 export default {
     name: "PostPage",
     data() {
         return {
-            apiBaseUrl: 'http://127.0.0.1:8000/api',
-            apiUrls: {
-                posts: '/posts'
-            },
+            // apiBaseUrl: 'http://127.0.0.1:8000/api',
+            // apiUrls: {
+            //     posts: '/posts'
+            // },
+            store,
             post: null,
             isError: false,
-            errorMessage: null
+            errorMessage: null,
+            formData: {
+                author: '',
+                content: ''
+            }
         }
     },
     methods: {
+        sendComment() {
+            console.log(this.formData.author);
+            console.log(this.formData.content);
+            console.log(this.post.id);
+
+            const data = {
+                author: this.formData.author,
+                content: this.formData.content,
+                post_id: this.post.id
+            };
+
+            axios.post(`${this.store.apiBaseUrl}/comments`, data)
+                .then((response) => {
+                    console.log(response)
+
+                    if(response.status === 201){ //created
+                        this.post.comments.push(response.data)
+                    } 
+
+
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+
+
+        },
+        commentDate(comment_date) {
+            const commentDate = new Date(comment_date);
+            return commentDate.toLocaleString();
+        },
         getPost() {
             axios.get(this.apiBaseUrl + this.apiUrls.posts + "/" + this.$route.params.slug)
                 .then((response) => {
@@ -66,6 +103,33 @@ export default {
             </router-link> -->
             <div class="my-3" v-show="post.technologies.length">
                 <span class="badge text-bg-warning me-1" v-for="technology in post.technologies">{{ technology.name }}</span>
+            </div>
+            <hr>
+            <div class="my-4">
+                <h3>Commenti</h3>
+                <ul v-if="post.comments.length">
+                    <li v-for="comment in post.comments">
+                        <div>
+                            <h4>{{ comment.author ?? 'Utente Anonimo' }}</h4>
+                            <p>{{ commentDate(comment.created_at) }}</p>
+                            <p>{{ comment.content }}</p>
+                        </div>
+                    </li>
+                </ul>
+                <div>
+                    <h3>Nuovo commento</h3>
+                    <form @submit.prevent="sendComment">
+                        <div class="mb-3">
+                            <label for="author" class="form-label">Autore</label>
+                            <input type="text" class="form-control" id="author" v-model="formData.author">
+                        </div>
+                        <div class="mb-3">
+                            <label for="content" class="form-label">Testo</label>
+                            <textarea class="form-control" id="content" rows="3" v-model="formData.content"></textarea>
+                        </div>
+                        <button class="btn btn-primary">Invia commento</button>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
